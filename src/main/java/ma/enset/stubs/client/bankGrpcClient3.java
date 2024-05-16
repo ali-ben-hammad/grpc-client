1108692b7fb33f17e80840d0f5b1fc8292f13d0a
ali-ben-hammad <ali.benhammad-etu@etu.univh2c.ma>
2024-05-16 13:25:37 +0100

    third client using serverStreaming

---
+++ src/main/java/ma/enset/stubs/client/bankGrpcClient3.java
@@ -0,0 +1,53 @@
+package ma.enset.stubs.client;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+import ma.enset.stubs.Bank;
+import ma.enset.stubs.bankServiceGrpc;
+
+import java.io.IOException;
+
+public class bankGrpcClient3 {
+    public static void main(String[] args) throws IOException {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 5555)
+                .usePlaintext()
+                .build();
+
+        bankServiceGrpc.bankServiceStub asyncStub = bankServiceGrpc.newStub(channel);
+
+        Bank.convertCurrencyRequest request = Bank.convertCurrencyRequest.newBuilder()
+                .setFrom("USD")
+                .setTo("MAD")
+                .setAmount(100)
+                .build();
+
+
+       asyncStub.getCurrencyStream(request, new StreamObserver<Bank.convertCurrencyResponse>() {
+                   @Override
+                   public void onNext(Bank.convertCurrencyResponse convertCurrencyResponse) {
+                       System.out.println("************");
+                       System.out.println(convertCurrencyResponse);
+                       System.out.println("************");
+                   }
+
+                   @Override
+                   public void onError(Throwable throwable) {
+                       System.out.println(throwable.getMessage());
+                       System.out.println("************");
+                   }
+
+                   @Override
+                   public void onCompleted() {
+                       System.out.println("************");
+                       System.out.println("End");
+                       System.out.println("************");
+                   }
+               });
+
+        System.out.println(".........?");
+        System.in.read();
+
+    }
+
+}
